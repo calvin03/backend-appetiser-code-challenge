6c5d76990a9a5201351533dc6299349f380f2b6d
Calvin Calvento <calvento.calvin@gmail.com>
2020-04-14 21:32:27 +0800

    initial

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Event;
+use DateTime;
 use Response;
 
 class EventController extends Controller
@@ -38,11 +39,12 @@ class EventController extends Controller
 
     public function addEvent(Request $request){
 
+        $to_date = new DateTime($request->eventForm['to']);
 
         $add_event = new Event();
         $add_event->title = $request->eventForm['event'];
         $add_event->start = $request->eventForm['from'];
-        $add_event->end = $request->eventForm['to'];
+        $add_event->end = $to_date->modify('+1 day');
 
         if($add_event->save()){
             return Response::json([
