97b025c8c2721b914d3f1d4f1ad237ab85b40808
Calvin Calvento <calvento.calvin@gmail.com>
2020-04-15 01:41:03 +0800

    initial commit

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -10,52 +10,55 @@ use Response;
 class EventController extends Controller
 {
 
-    public function getAllEvents(){
+    public function getAllEvents()
+    {
 
-        $all_events = Event::all();
+        try {
 
-        if(count($all_events) > 0){
+            $all_events = Event::all();
+
+            if (count($all_events) > 0) {
+
+                return Response::json([
+                    'data' => $all_events
+                ], 201);
+            } else {
+                return Response::json([
+                    'data' => "No Event Found"
+                ], 200);
+            }
+        } catch (\Exception $e) {
 
             return Response::json([
-            'data' => $all_events
-            ], 201);
-        }
-
-        else {
-            return Response::json([
-                'data' => "No Event Found"
+                'message' => $e->getMessage()
             ], 200);
         }
-
-
-
-
-
-
-
-
-
     }
 
-    public function addEvent(Request $request){
+    public function addEvent(Request $request)
+    {
+        try {
+            $to_date = new DateTime($request->eventForm['to']);
 
-        $to_date = new DateTime($request->eventForm['to']);
+            $add_event = new Event();
+            $add_event->title = $request->eventForm['event'];
+            $add_event->start = $request->eventForm['from'];
+            $add_event->end = $to_date->modify('+1 day');
 
-        $add_event = new Event();
-        $add_event->title = $request->eventForm['event'];
-        $add_event->start = $request->eventForm['from'];
-        $add_event->end = $to_date->modify('+1 day');
+            if ($add_event->save()) {
+                return Response::json([
+                    'data' => "Add Event Successful"
+                ], 200);
+            } else {
+                return Response::json([
+                    'data' => "Add Event Not Added"
+                ], 200);
+            }
+        } catch (\Exception $e) {
 
-        if($add_event->save()){
             return Response::json([
-                'data' => "Add Event Successful"
+                'message' => $e->getMessage()
             ], 200);
         }
-        else{
-            return Response::json([
-                'data' => "Add Event Not Added"
-            ], 200);
-        }
-
     }
 }
